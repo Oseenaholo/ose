b03136652a2ed2148adc3016ecb3d3cb325ff39e
Oseenaholo <osemudiamen.enaholo@gmail.com>
2021-08-06 18:48:00 -0500

    Update configureDB.sh

--- linux/configureDB.sh
+++ linux/configureDB.sh
@@ -2,7 +2,7 @@
 
 # Configure Database
 
-mysql
+sudo mysql
 CREATE DATABASE ecomdb;
 CREATE USER 'ecomuser'@'localhost' IDENTIFIED BY 'ecompassword';
 GRANT ALL PRIVILEGES ON *.* TO 'ecomuser'@'localhost';
